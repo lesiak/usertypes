817085021aa9e0c615b98be60e3a94fbecb36d3d
lesiak <lesiak@outlook.com>
2025-06-26 16:30:01 +0200

    Simplified User types

--- src/main/java/com/lesiak/test/usertypes/usertypes/FooIdUserType.java
+++ src/main/java/com/lesiak/test/usertypes/usertypes/FooIdUserType.java
@@ -1,9 +1,17 @@
 package com.lesiak.test.usertypes.usertypes;
 
 import com.lesiak.test.usertypes.entities.FooId;
+import io.hypersistence.utils.hibernate.type.DescriptorImmutableType;
+import org.hibernate.HibernateException;
+import org.hibernate.type.descriptor.jdbc.VarcharJdbcType;
 
-public class FooIdUserType extends StringWrapperUserType<FooId> {
+public class FooIdUserType extends DescriptorImmutableType<FooId, VarcharJdbcType, FooIdTypeDescriptor> {
     public FooIdUserType() {
-        super(FooId.class);
+        super(FooId.class, VarcharJdbcType.INSTANCE, FooIdTypeDescriptor.INSTANCE);
+    }
+
+    public FooId fromStringValue(CharSequence sequence) throws HibernateException {
+        return this.getExpressibleJavaType().wrap(sequence, null);
     }
 }
+
