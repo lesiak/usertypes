94c80f0f18078b2288b9a4b20278d1b6af8e3f00
lesiak <lesiak@outlook.com>
2025-07-08 11:28:33 +0200

    Custom Dialect

---
+++ src/main/java/com/lesiak/test/usertypes/dialect/MyPostgreSQLDialect.java
@@ -0,0 +1,11 @@
+package com.lesiak.test.usertypes.dialect;
+
+import org.hibernate.dialect.PostgreSQLDialect;
+
+public class MyPostgreSQLDialect extends PostgreSQLDialect {
+    @Override
+    public boolean useArrayForMultiValuedParameters() {
+        return false;
+    }
+
+}
